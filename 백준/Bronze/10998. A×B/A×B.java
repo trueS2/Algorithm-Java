020881ba64fb5ec38fa043dfc0d5e96476d5bccb
trueS2 <166217831+trueS2@users.noreply.github.com>
2024-11-06 16:25:42 +0900

    [Bronze V] Title: A×B, Time: 172 ms, Memory: 17684 KB -BaekjoonHub

---
+++ 백준/Bronze/10998. A×B/A×B.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args){
+        Scanner Sc = new Scanner(System.in);
+
+        int A = Sc.nextInt();
+        int B = Sc.nextInt();
+
+        System.out.println(A * B);
+    }
+}
