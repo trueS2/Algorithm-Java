e04744c6ce50453e341c083b94d1d5450073494a
trueS2 <166217831+trueS2@users.noreply.github.com>
2024-11-06 16:38:39 +0900

    [Bronze V] Title: A/B, Time: 176 ms, Memory: 17808 KB -BaekjoonHub

---
+++ 백준/Bronze/1008. A／B/A／B.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args){
+        Scanner Sc = new Scanner(System.in);
+
+        double A = Sc.nextInt();
+        double B = Sc.nextInt();
+
+        System.out.println(A / B);
+    }
+}
