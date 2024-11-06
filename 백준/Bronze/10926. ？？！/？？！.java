0d6ae697d0b1301d53cd132095a60a99eeda4e21
trueS2 <166217831+trueS2@users.noreply.github.com>
2024-11-06 16:49:59 +0900

    [Bronze V] Title: ??!, Time: 172 ms, Memory: 17728 KB -BaekjoonHub

---
+++ 백준/Bronze/10926. ？？！/？？！.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args){
+        Scanner Sc = new Scanner(System.in);
+
+        String Input = Sc.nextLine();
+
+        System.out.println(Input + "??!");
+    }
+}
