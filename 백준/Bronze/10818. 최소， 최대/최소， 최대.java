ec44377a6d543acdfae6b2c41d655da905b0dda2
trueS2 <166217831+trueS2@users.noreply.github.com>
2024-12-04 21:59:33 +0900

    [Bronze III] Title: 최소, 최대, Time: 1568 ms, Memory: 310632 KB -BaekjoonHub

---
+++ 백준/Bronze/10818. 최소， 최대/최소， 최대.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int N = scanner.nextInt();
+
+        int min = Integer.MAX_VALUE;
+        int max = Integer.MIN_VALUE;
+
+        for (int i = 0; i < N; i++) {
+            int num = scanner.nextInt();
+            if (num < min) {
+                min = num;
+            }
+            if (num > max) {
+                max = num;
+            }
+        }
+
+        System.out.println(min + " " + max);
+    }
+}
