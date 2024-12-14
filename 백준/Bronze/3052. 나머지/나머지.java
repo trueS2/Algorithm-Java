eea15dc96368ec4c12c5dc1f9e900868392279a0
trueS2 <166217831+trueS2@users.noreply.github.com>
2024-12-14 17:11:55 +0900

    [Bronze II] Title: 나머지, Time: 172 ms, Memory: 17696 KB -BaekjoonHub

---
+++ 백준/Bronze/3052. 나머지/나머지.java
@@ -0,0 +1,16 @@
+import java.util.HashSet;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        HashSet<Integer> remainders = new HashSet<>();
+        
+        for (int i = 0; i < 10; i++) {
+            int num = sc.nextInt();
+            remainders.add(num % 42);
+        }
+        
+        System.out.println(remainders.size());
+    }
+}
