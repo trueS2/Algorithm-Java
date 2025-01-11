c7f36aeb4e19f450bcf0dc102df00a8bbceec818
trueS2 <166217831+trueS2@users.noreply.github.com>
2025-01-11 21:24:11 +0900

    [Bronze III] Title: 그대로 출력하기, Time: 188 ms, Memory: 17932 KB -BaekjoonHub

---
+++ 백준/Bronze/11718. 그대로 출력하기/그대로 출력하기.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args){
+
+        Scanner sc = new Scanner(System.in);
+
+        while (sc.hasNextLine()) {
+            String N = sc.nextLine();
+            System.out.println(N);
+        }
+    }
+}
