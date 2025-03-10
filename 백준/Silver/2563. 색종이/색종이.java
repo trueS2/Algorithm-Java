2f8b3cac4034a9a92f331fe9563afbe753065c41
trueS2 <166217831+trueS2@users.noreply.github.com>
2025-03-10 21:23:51 +0900

    [Silver V] Title: 색종이, Time: 176 ms, Memory: 17648 KB -BaekjoonHub

---
+++ 백준/Silver/2563. 색종이/색종이.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        
+        boolean [] [] paper = new boolean[100][100];
+        int N = sc.nextInt();
+        
+        int area = 0;
+        
+        for ( int i = 0; i < N; i++){
+            int x = sc.nextInt();
+            int y = sc.nextInt();
+            
+            for ( int dx = x; dx < x + 10; dx++){
+                for ( int dy = y; dy < y + 10; dy++){
+                    if ( !paper[dx][dy]){
+                        paper[dx][dy] = true;
+                        area++;
+                    }
+                }
+            }
+        }
+        
+        System.out.println(area);
+        sc.close();
+    }
+}
