357102a996d4a231ddc0306b70b63c7722ae37bf
trueS2 <166217831+trueS2@users.noreply.github.com>
2025-03-14 14:42:16 +0900

    [Silver V] Title: 좌표 정렬하기 2, Time: 1912 ms, Memory: 185952 KB -BaekjoonHub

---
+++ 백준/Silver/11651. 좌표 정렬하기 2/좌표 정렬하기 2.java
@@ -0,0 +1,30 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        int N = sc.nextInt();
+        int [][] points = new int[N][2];
+
+        for ( int i = 0; i < N; i++){
+            points[i][0] = sc.nextInt();
+            points[i][1] = sc.nextInt();
+        }
+
+        Arrays.sort(points, (a, b) -> {
+            if (a[1] == b[1]) {
+                return Integer.compare(a[0], b[0]);
+            }
+            return Integer.compare(a[1], b[1]);
+        });
+
+        for (int i = 0; i < N; i++) {
+            System.out.println(points[i][0] + " " + points[i][1]);
+        }
+
+        sc.close();
+    }
+}
