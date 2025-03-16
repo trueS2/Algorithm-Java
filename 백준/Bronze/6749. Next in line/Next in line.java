4c32019362f96bad739c6ee3ad10b5777d98b0b3
trueS2 <166217831+trueS2@users.noreply.github.com>
2025-03-16 20:06:05 +0900

    [Bronze IV] Title: Next in line, Time: 172 ms, Memory: 17696 KB -BaekjoonHub

---
+++ 백준/Bronze/6749. Next in line/Next in line.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        
+        Scanner sc = new Scanner(System.in);
+        
+        int y = sc.nextInt();
+        int m = sc.nextInt();
+        
+        System.out.println(2 * m - y);
+        sc.close();
+    }
+}
