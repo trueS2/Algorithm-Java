2b485a010e75bbdf200445bd31b9c7f9bc9e7b74
trueS2 <166217831+trueS2@users.noreply.github.com>
2025-03-28 20:44:46 +0900

    [Bronze V] Title: 삼각형, Time: 176 ms, Memory: 17768 KB -BaekjoonHub

---
+++ 백준/Bronze/29751. 삼각형/삼각형.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        
+        Scanner sc = new Scanner(System.in);
+        
+        int W = sc.nextInt();
+        int H = sc.nextInt();
+        
+        double area = (W * H) / 2.0;
+        
+        System.out.printf("%.1f", area);
+        sc.close();
+    }
+}
