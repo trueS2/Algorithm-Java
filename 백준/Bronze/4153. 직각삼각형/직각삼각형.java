be44a0899d821f7b2cf3431c5dc03b37755be1de
trueS2 <166217831+trueS2@users.noreply.github.com>
2025-04-01 23:15:14 +0900

    [Bronze III] Title: 직각삼각형, Time: 176 ms, Memory: 17648 KB -BaekjoonHub

---
+++ 백준/Bronze/4153. 직각삼각형/직각삼각형.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args) {
+        
+        Scanner sc = new Scanner(System.in);
+        
+        while (true) {
+            int a = sc.nextInt();
+            int b = sc.nextInt();
+            int c = sc.nextInt();
+            
+            if (a == 0 && b == 0 && c == 0) {
+                break;
+            }
+
+            int[] sides = {a, b, c};
+            Arrays.sort(sides);
+            
+            if (sides[0] * sides[0] + sides[1] * sides[1] == sides[2] * sides[2]) {
+                System.out.println("right");
+            } else {
+                System.out.println("wrong");
+            }
+        }
+        
+        sc.close();
+    }
+}
