441372e7a76e6e2b0df8991ed2cb7b2e9b7e4bd0
trueS2 <166217831+trueS2@users.noreply.github.com>
2025-04-21 19:28:00 +0900

    [Silver II] Title: DNA 비밀번호, Time: 304 ms, Memory: 23356 KB -BaekjoonHub

---
+++ 백준/Silver/12891. DNA 비밀번호/DNA 비밀번호.java
@@ -0,0 +1,80 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    static int[] checkArr = new int[4];
+    static int[] myArr = new int[4];
+    static int checkSecret = 0;
+
+    public static void main(String[] args) throws IOException {
+        
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        
+        int S = Integer.parseInt(st.nextToken());
+        int P = Integer.parseInt(st.nextToken());
+        char[] A = br.readLine().toCharArray();
+        
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < 4; i++) {
+            checkArr[i] = Integer.parseInt(st.nextToken());
+            if (checkArr[i] == 0) checkSecret++;
+        }
+
+        int result = 0;
+        for (int i = 0; i < P; i++) add(A[i]);
+        if (checkSecret == 4) result++;
+
+        for (int i = P; i < S; i++) {
+            int j = i - P;
+            add(A[i]);
+            remove(A[j]);
+            if (checkSecret == 4) result++;
+        }
+
+        System.out.println(result);
+        br.close();
+    }
+
+    private static void add(char c) {
+        switch (c) {
+            case 'A':
+                myArr[0]++;
+                if (myArr[0] == checkArr[0]) checkSecret++;
+                break;
+            case 'C':
+                myArr[1]++;
+                if (myArr[1] == checkArr[1]) checkSecret++;
+                break;
+            case 'G':
+                myArr[2]++;
+                if (myArr[2] == checkArr[2]) checkSecret++;
+                break;
+            case 'T':
+                myArr[3]++;
+                if (myArr[3] == checkArr[3]) checkSecret++;
+                break;
+        }
+    }
+
+    private static void remove(char c) {
+        switch (c) {
+            case 'A':
+                if (myArr[0] == checkArr[0]) checkSecret--;
+                myArr[0]--;
+                break;
+            case 'C':
+                if (myArr[1] == checkArr[1]) checkSecret--;
+                myArr[1]--;
+                break;
+            case 'G':
+                if (myArr[2] == checkArr[2]) checkSecret--;
+                myArr[2]--;
+                break;
+            case 'T':
+                if (myArr[3] == checkArr[3]) checkSecret--;
+                myArr[3]--;
+                break;
+        }
+    }
+}
