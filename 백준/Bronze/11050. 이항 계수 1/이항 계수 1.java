914f87acc9b59bb976c217c63c1cd3bc5ce810ef
trueS2 <166217831+trueS2@users.noreply.github.com>
2025-05-27 23:16:58 +0900

    [Bronze I] Title: 이항 계수 1, Time: 172 ms, Memory: 17716 KB -BaekjoonHub

---
+++ 백준/Bronze/11050. 이항 계수 1/이항 계수 1.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        
+        Scanner sc = new Scanner(System.in);
+        
+        int n = sc.nextInt();
+        int k = sc.nextInt();
+        
+        System.out.println(factorial(n) / (factorial(k) * factorial(n - k)));
+    }
+
+    static int factorial(int x) {
+        int result = 1;
+        for (int i = 2; i <= x; i++) {
+            result *= i;
+        }
+        return result;
+    }
+}
