3a3888329df2bdc68732049c3420f7d5576a173c
trueS2 <166217831+trueS2@users.noreply.github.com>
2025-05-29 23:36:11 +0900

    [Silver IV] Title: 보물, Time: 180 ms, Memory: 17788 KB -BaekjoonHub

---
+++ 백준/Silver/1026. 보물/보물.java
@@ -0,0 +1,29 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        
+        Scanner sc = new Scanner(System.in);
+        
+        int n = sc.nextInt();
+        Integer[] a = new Integer[n];
+        Integer[] b = new Integer[n];
+        
+        for (int i = 0; i < n; i++) {
+            a[i] = sc.nextInt();
+        }
+        
+        for (int i = 0; i < n; i++) {
+            b[i] = sc.nextInt();
+        }
+        
+        Arrays.sort(a);
+        Arrays.sort(b, Collections.reverseOrder());
+        
+        int result = 0;
+        for (int i = 0; i < n; i++) {
+            result += a[i] * b[i];
+        }
+        System.out.println(result);
+    }
+}
